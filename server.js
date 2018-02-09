1bbb97e24ef73f4089e341730ccd184d455bf553
Ómar Óskarsson <omar.oskarsson@advania.is>
2018-02-09 18:55:40 +0000

    connect to sqlite db

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
+import compression from 'compression';
+import { Engine } from 'apollo-engine';
+
 import schema from './data/schema';
 
 const GRAPHQL_PORT = 3000;
+const ENGINE_API_KEY = 'service:omaroskars-4602:MPAo06lsP_blbGjtcgxmPg'; // TODO
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+const engine = new Engine({
+  engineConfig: {
+    apiKey: ENGINE_API_KEY
+  },
+  graphqlPort: GRAPHQL_PORT
+});
+
+engine.start();
+
+// This must be the first middleware
+graphQLServer.use(engine.expressMiddleware());
+graphQLServer.use(compression());
+graphQLServer.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    // This option turns on tracing
+    tracing: true,
+    cacheControl: true
+  })
+);
+
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 graphQLServer.listen(GRAPHQL_PORT, () =>
